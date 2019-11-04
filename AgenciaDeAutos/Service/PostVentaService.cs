8c088f8b2baba0585d3a6c608ed78022dbc4538d
cabezonbhh <53849259+cabezonbhh@users.noreply.github.com>
2019-11-04 15:00:01 -0300

    pequeños cambios

--- AgenciaDeAutos/Service/PostVentaService.cs
+++ AgenciaDeAutos/Service/PostVentaService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AgenciaDeAutos.Business;
+using AgenciaDeAutos.Data.Dao;
+using AgenciaDeAutos.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,18 @@ namespace AgenciaDeAutos.Service
 {
     public class PostVentaService
     {
+        private IDaoPostVenta<PostVenta> dao;
+        public int ClienteSeleccionado { get; set; }
+
+        public PostVentaService()
+        {
+            dao = new PostVentaDaoSqlImp();
+        }
+
+        public IList<PostVenta> GetPostVentasPorID(int id)
+        {
+            return dao.getPostVentaPorIdUnidad(id,"","",1);
+        }
 
     }
 }
